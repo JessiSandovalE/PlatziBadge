94aaea930d952e19a18328aefa99e495664b3c6a
jessica <jessi.sandoval93@gmail.com>
2020-10-19 09:38:53 -0500

    add Page Loading, Page error use get api

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -1,10 +1,11 @@
 import React from 'react';
 
 import './styles/BadgeNew.css'
-import header from '../images/badge-header.svg'
+import header from '../images/platziconf-logo.svg'
 
 import BadgeForm from '../components/BadgeForm'
 import Badge from '../components/Badge'
+import api from '../api'
 
 class BadgeNew  extends React.Component {
     state = {form: {
@@ -13,7 +14,7 @@ class BadgeNew  extends React.Component {
         email: '',
         jobTitle: '',
         twitter: ''
-    }}
+    }} 
 
     handleChange = e => {
         const nextForm = this.state.form
@@ -25,25 +26,40 @@ class BadgeNew  extends React.Component {
             }
         })
     }
+
+    handleSubmit = async e => {
+        e.preventDefault  ()
+
+        try {
+            await api.badge.create(this.state.form)
+            this.setState ({ loading: false })
+        } catch (error) {
+            this.setState({ loading: false, error: error })
+        }
+    }
     render(){
         return (
             <React.Fragment>
                 <div className="BadgeNew__hero">
-                    <img className="img-fluid" src={header} alt="logo"/>
+                    <img className="BadgeNew__hero-image img-fluid" src={header} alt="logo"/>
                 </div>
                 <div className="container">
                     <div className="row">
                         <div className="col-6">
                             <Badge
-                            firstName={this.state.form.firstName}
-                            lastName={this.state.form.lastName}
-                            jobTitle={this.state.form.jobTitle}
-                            twitter ={this.state.form.twitter}
-                            email = {this.state.form.email}
+                            firstName={this.state.form.firstName || 'FIRST_NAME'}
+                            lastName={this.state.form.lastName || 'LAST_NAME'}
+                            jobTitle={this.state.form.jobTitle || 'JOB_TITLE'}
+                            twitter ={this.state.form.twitter || 'twitter'}
+                            email = {this.state.form.email || 'email'}
                             avatarUrl="https://s.gravatar.com/avatar/207ba66ecbc980c1d7410e2e2eb2d957?s=80"/>
                         </div>
                         <div className="col-6">
-                            <BadgeForm  onChange = {this.handleChange} formValues={this.state.form} />
+                            <BadgeForm  
+                                onChange = {this.handleChange}
+                                onSubmit = { this.handleSubmit}
+                                formValues={this.state.form} 
+                            />
                         </div>
                     </div>
                 </div>
--- src/pages/Badges.js
+++ src/pages/Badges.js
@@ -4,10 +4,31 @@ import { Link } from 'react-router-dom'
 import './styles/Badges.css'
 import ConfLogo from '../images/badge-header.svg'
 import BadgesList from '../components/BadgesList'
+import PageLoading from '../components/PageLoading'
+import PageError from '../components/PageError'
+import api from '../api'
 
 class Badges extends React.Component {
+    state = {
+        loading: true,
+        error: null,
+        data: undefined
+    }
+    componentDidMount(){
+        this.fetchData()
+    }
 
-    constructor(props){
+    fetchData = async() => {
+       this.setState({ loading: true, error: null })
+
+       try {
+           const data = await api.badges.list()
+           this.setState ({ loading: false , data: data})
+       } catch (error) {
+           this.setState ({ loading: false, error: error })
+       }
+    }
+   /*  constructor(props){
         super(props)
         console.log('1. constructor()')
         this.state = {
@@ -20,38 +41,7 @@ class Badges extends React.Component {
 
         this.timeoutId = setTimeout(() =>{
             this.setState({
-                data:[
-                    {
-                      id: '2de30c42-9deb-40fc-a41f-05e62b5939a7',
-                      firstName: 'Freda',
-                      lastName: 'Grady',
-                      email: 'Leann_Berge@gmail.com',
-                      jobTitle: 'Legacy Brand Director',
-                      twitter: 'FredaGrady22221-7573',
-                      avatarUrl:
-                        'https://www.gravatar.com/avatar/f63a9c45aca0e7e7de0782a6b1dff40b?d=identicon',
-                    },
-                    {
-                      id: 'd00d3614-101a-44ca-b6c2-0be075aeed3d',
-                      firstName: 'Major',
-                      lastName: 'Rodriguez',
-                      email: 'Ilene66@hotmail.com',
-                      jobTitle: 'Human Research Architect',
-                      twitter: 'MajorRodriguez61545',
-                      avatarUrl:
-                        'https://www.gravatar.com/avatar/d57a8be8cb9219609905da25d5f3e50a?d=identicon',
-                    },
-                    {
-                      id: '63c03386-33a2-4512-9ac1-354ad7bec5e9',
-                      firstName: 'Daphney',
-                      lastName: 'Torphy',
-                      email: 'Ron61@hotmail.com',
-                      jobTitle: 'National Markets Officer',
-                      twitter: 'DaphneyTorphy96105',
-                      avatarUrl:
-                        'https://www.gravatar.com/avatar/e74e87d40e55b9ff9791c78892e55cb7?d=identicon',
-                    },
-                  ]
+                data:undefined
             })
         },3000)
     }
@@ -72,9 +62,16 @@ class Badges extends React.Component {
     componentWillUnmount(){
         console.log('6.componentWillUnmount')
         clearTimeout(this.timeoutId)
-    }
+    } */
     render(){
-        console.log('2/4. render()')
+        /* console.log('2/4. render()') */
+        if (this.state.loading === true){
+            return <PageLoading />
+        }
+
+        if(this.state.error){
+            return  <PageError error={ this.state.error} />
+        }
         return(
             <React.Fragment>
                 <div className="Badges">
